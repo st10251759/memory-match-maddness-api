467dc1924c24ed22ace25d3ba8534021f7c4f107
Cameron Chetty <ST10251759@vcconnect.edu.za>
2025-10-03 11:49:39 +0200

    Updated Server.js

--- server.js
+++ server.js
@@ -8,6 +8,7 @@ const userRoutes = require('./src/routes/userRoutes');
 const gameRoutes = require('./src/routes/gameRoutes');
 const achievementRoutes = require('./src/routes/achievementRoutes');
 const themeRoutes = require('./src/routes/themeRoutes');
+const multiplayerRoutes = require('./src/routes/multiplayerRoutes');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -23,6 +24,7 @@ app.use('/api/users', userRoutes);
 app.use('/api/games', gameRoutes);
 app.use('/api/achievements', achievementRoutes);
 app.use('/api/themes', themeRoutes);
+app.use('/api/multiplayer', multiplayerRoutes);
 
 // Health check
 app.get('/api/health', (req, res) => {
